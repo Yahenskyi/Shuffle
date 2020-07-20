f93ff59447399a026e9b4063d701536094499d27
Volodymyr Yahenskyi <48393900+Yahenskyi@users.noreply.github.com>
2020-07-20 20:35:25 +0300

    Update CardStackTransformProvider.swift

--- Sources/Shuffle/SwipeCardStack/CardStackTransformProvider.swift
+++ Sources/Shuffle/SwipeCardStack/CardStackTransformProvider.swift
@@ -59,10 +59,10 @@ class CardStackTransformProvider: CardStackTransformProvidable {
                                    currentPosition: Int) -> CGFloat {
     let percentage = backgroundCardTransformPercentage(for: cardStack, topCard: topCard)
     
-    if percentage >= 0.3 {
+    if percentage >= 0.25 {
         return percentage
     } else {
-        return 0.3
+        return 0.25
     }
     
   }
